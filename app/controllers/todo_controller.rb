4001e6e2e562a3aeac5d48c79ede21457d697eef
Maria Salcido <maria2513.gsalcido@gmail.com>
2017-10-22 15:05:44 +0000

    params_id/db

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,36 +3,37 @@ class TodoController < ApplicationController
     end
 
     def show
-        if params[:id]=="1"
-            @todo_description = "Make the curriculum"
-            @todo_pomodoro_estimate = 4
+        @my_todo = Todo.find_by_id(params[:id])
+        # if params[:id]=="1"
+        #     @todo_description = "Make the curriculum"
+        #     @todo_pomodoro_estimate = 4
             
-        elsif params[:id]=="2"
-            @todo_description = "Buy workshop supplies"
-            @todo_pomodoro_estimate = 3
+        # elsif params[:id]=="2"
+        #     @todo_description = "Buy workshop supplies"
+        #     @todo_pomodoro_estimate = 3
             
-        elsif params[:id]=="3"
-            @todo_description = "Meet with the volunteer trainers"
-            @todo_pomodoro_estimate = 2
+        # elsif params[:id]=="3"
+        #     @todo_description = "Meet with the volunteer trainers"
+        #     @todo_pomodoro_estimate = 2
             
-        elsif params[:id]=="4"
-            @todo_description = "Order food for Saturday and Sunday"
-            @todo_pomodoro_estimate = 1
+        # elsif params[:id]=="4"
+        #     @todo_description = "Order food for Saturday and Sunday"
+        #     @todo_pomodoro_estimate = 1
             
-        elsif params[:id]=="5"
-            @todo_description = "Check pre-work assignments"
-            @todo_pomodoro_estimate = 2
+        # elsif params[:id]=="5"
+        #     @todo_description = "Check pre-work assignments"
+        #     @todo_pomodoro_estimate = 2
             
-        elsif params[:id]=="6"
-            @todo_description = "Send workshop location to all students"
-            @todo_pomodoro_estimate = 1
+        # elsif params[:id]=="6"
+        #     @todo_description = "Send workshop location to all students"
+        #     @todo_pomodoro_estimate = 1
             
-        elsif params[:id]=="7"
-            @todo_description = "Have a great workshop"
-            @todo_pomodoro_estimate = 0
+        # elsif params[:id]=="7"
+        #     @todo_description = "Have a great workshop"
+        #     @todo_pomodoro_estimate = 0
             
             
         
-        end
+        # end
     end
 end 
