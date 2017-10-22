1cd5538f9a1d8a73884adbce5e89812c86764674
Maria Salcido <maria2513.gsalcido@gmail.com>
2017-10-22 16:45:07 +0000

    Working Add a todo button

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -8,5 +8,14 @@ class TodoController < ApplicationController
     
     def new
     end    
+    
+    def create
+        t = Todo.new
+        t.description = params[:description]
+        t.pomodoro_estimate = params[:pomodoro_estimate]
+        t.complete = false 
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+    end
 end 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
   get 'todo/show/:id' , to: 'todo#show'
   get 'todo/new/', to: 'todo#new'
-
+  post'todo/create',to: 'todo#create'
 end
